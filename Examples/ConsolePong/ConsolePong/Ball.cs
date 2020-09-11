2c3c89807abd9130a3cdc8df477c9f2bcc7686bd
Daxanius <38554396+Daxanius@users.noreply.github.com>
2020-09-11 22:47:27 +0200

    Upload example ConsolePong

---
+++ Examples/ConsolePong/ConsolePong/Ball.cs
@@ -0,0 +1,23 @@
+﻿using FishcomGL;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ConsolePong
+{
+    class Ball
+    {
+        public Vector2 position;
+
+        public Ball(Vector2 position)
+        {
+            this.position = position;
+        }
+
+        public void Draw(Window window)
+        {
+            window.SetPixel(position, 'X', "RED", "RED");
+        }
+    }
+}
